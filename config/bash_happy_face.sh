fb2366665cb25fb039cb3afad62db9414c721001
Andrey Rybak <rybak.a.v@gmail.com>
2015-11-15 16:30:08 +0300

    happy-face : make SADNESS local

--- config/bash_happy_face.sh
+++ config/bash_happy_face.sh
@@ -14,11 +14,10 @@ function __sad_ps1() {
 
     local PREF="$1"
     local POST="$2"
+    local SADNESS=
     if [[ "$EXIT" != '0' ]]
     then
         SADNESS="$SMILEY"
-    else
-        SADNESS=''
     fi
     local GIT_POST="${SADNESS}${POST}"
     __git_ps1 "$PREF" "$GIT_POST"

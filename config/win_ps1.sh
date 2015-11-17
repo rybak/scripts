3b593c6631345c7888eb2ad6845c315ee7081a11
Andrey Rybak <rybak.a.v@gmail.com>
2015-11-17 19:03:39 +0300

    add unset for font variables

--- config/win_ps1.sh
+++ config/win_ps1.sh
@@ -11,15 +11,16 @@ source "$HOME/scripts/lib/colors.sh"
 #PS1="$PS1"'\n'                 # new line
 # Do not PS1="$PS1"'\[\033[32m\]'      # change to green
 
-USERNAME_FONT='\e[0;33;93m'
-HOSTNAME_FONT='\e[0;32m'
-
 ORIG_PS1="$PS1" # store default PS1
 
+USERNAME_FONT='\e[0;33;93m'
 PS1="\[$USERNAME_FONT\]"'\u' # user
 PS1="$PS1""\[$WHITE_FG\]"'@'  # @
+HOSTNAME_FONT='\e[0;32m'
 PS1="$PS1""\[$HOSTNAME_FONT\]"'\h ' # host<space>
 ps1_reset_font
 PS1="$PS1"'\[\033[01;34m\]''\w/' # current working directory<slash>
 ps1_reset_font
 
+unset USERNAME_FONT
+unset HOSTNAME_FONT

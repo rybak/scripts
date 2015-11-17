9286e5af01d9094faf7b60aad107e2a626661f7d
Andrey Rybak <rybak.a.v@gmail.com>
2015-11-17 18:48:51 +0300

    fix readme notification

--- config/ps1_notifications.sh
+++ config/ps1_notifications.sh
@@ -25,7 +25,7 @@ function __sad_ps1() {
     fi
     if [[ -f 'README' || -f 'README.md' || -f 'readme.txt' || -f 'readme.md' ]]
     then
-        POST_NOTIFY='\['$BLUE_FG'\]📖 $PS_RESET_FONT'
+        POST_NOTIFY='\['$BLUE_FG'\]📖 '"$PS_RESET_FONT"
         POST_RESET="$PS_RESET_FONT"
     fi
 

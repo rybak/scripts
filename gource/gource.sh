c813d9c5c73e82e9298f2f3a1619973dcbd27e59
Andrei Rybak <rybak.a.v@gmail.com>
2018-06-12 18:34:28 +0200

    Add more customization to gource.sh

--- gource/gource.sh
+++ gource/gource.sh
@@ -2,12 +2,22 @@
 
 IMAGE_DIR="$HOME/Pictures/gource"
 TARGET="$1"
-CAPTIONS="$2"
+shift
+CAPTIONS="$1"
+shift
+USER_SCALE="${1:-4}"
+shift
 
-gource "$TARGET" \
+RANDOM=$$   # Reseed using script's PID
+
+# show since 80% to show more recent history
+r1=$((80+${RANDOM}%20))
+printf -v POSITION "0.%.2d" $r1
+
+gource "$TARGET" "$@" \
 	--fullscreen \
-	--start-position random --loop \
-	--hide bloom,filenames,usernames --user-scale 4 \
+	--start-position "$POSITION" --loop \
+	--hide bloom,filenames,usernames --user-scale "$USER_SCALE" \
 	--seconds-per-day 1 --time-scale 1 --auto-skip-seconds 1 \
 	--user-image-dir "$IMAGE_DIR" --logo "$IMAGE_DIR/logo.png" \
-	--caption-file "$CAPTIONS" --caption-duration 120 --caption-size 72 \
+	--caption-file "$CAPTIONS" --caption-duration 30 --caption-size 64 \

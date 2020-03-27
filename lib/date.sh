1eb4470945c7bf003e94ca66882a87fbf2337a57
Andrei Rybak <rybak.a.v@gmail.com>
2020-03-27 09:07:04 +0100

    lib/date: switch to YYYY format

--- lib/date.sh
+++ lib/date.sh
@@ -2,11 +2,11 @@
 
 # filesystem/typing-friendly variations of datetime output
 date_minutes () {
-	date +%y-%m-%dT%H-%M
+	date +%Y-%m-%dT%H-%M
 }
 
 date_seconds () {
-	date +%y-%m-%dT%H-%M-%S
+	date +%Y-%m-%dT%H-%M-%S
 }
 
 date_unix () {

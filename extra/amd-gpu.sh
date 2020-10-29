22948a957197d3a49aa8d88bc566e56beb89c8e5
Andrei Rybak <rybak.a.v@gmail.com>
2020-10-29 22:08:51 +0100

    amd-gpu: add glxheads

--- extra/amd-gpu.sh
+++ extra/amd-gpu.sh
@@ -2,3 +2,6 @@ lspci -nn | grep -E 'VGA|Display'
 grep -i switcheroo /boot/config-*
 sudo cat /sys/kernel/debug/vgaswitcheroo/switch
 sudo lshw -C video -C cpu
+glxheads
+# https://wiki.archlinux.org/index.php/PRIME
+DRI_PRIME=1 glxheads

f8f0a566b6090d44a878ad1f17145c37aa9f3c76
Andrei Rybak <rybak.a.v@gmail.com>
2022-10-04 11:31:15 +0200

    check_wikitext: automatically download category

--- wikipedia/check_wikitext.sh
+++ wikipedia/check_wikitext.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
-# For every English Wikipedia page in `input.txt`, prints first 20 characters of
-# its wikitext
+# For every English Wikipedia page in category `$1`, prints a bunch of wikitext
+# from the start and from the end of the page.
 
 BASE_HOST=en.wikipedia.org
 
-for i in $(cat input.txt)
+if [[ $# -lt 1 ]]
+then
+	echo "Missing category name"
+	exit 1
+fi
+
+category="${1// /_}"
+
+curl --silent "https://$BASE_HOST/w/api.php?action=query&generator=categorymembers&gcmtitle=Category:${category}&cllimit=max&gcmlimit=max&format=json" |
+	jq --raw-output '.query.pages | map(.title)[]' |
+	grep '^Template:' |
+while read i
 do
-	wikitext=$(curl -q "https://${BASE_HOST}/w/api.php?action=parse&format=json&page=${i}&prop=wikitext&formatversion=2" | jq '.parse.wikitext')
-	echo -e "$i\t${wikitext:0:20}"
-	sleep 3s
+	escaped=${i// /_}
+	URL="https://${BASE_HOST}/w/api.php?action=parse&format=json&page=${escaped}&prop=wikitext&formatversion=2" 
+	wikitext=$(curl --silent "$URL" | jq --raw-output '.parse.wikitext')
+	echo -e "$i"
+	echo -e "\t$(echo ${wikitext:0:100} | grep -o '[{][{] *[^|}]*')"
+	echo -e "\t${wikitext: -20}"
+	echo "-------------------------"
+	sleep 1s
 done

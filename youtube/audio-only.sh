4c9778d70638589a2694aae78f236291c568c41d
Andrei Rybak <rybak.a.v@gmail.com>
2023-06-10 13:28:23 +0200

    youtube/audio-only: convert to .opus automatically

--- youtube/audio-only.sh
+++ youtube/audio-only.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-yt-dlp --format bestaudio --continue --ignore-errors --no-overwrites --output '%(uploader_id)s/%(upload_date)s_%(title)s_%(id)s.%(ext)s' "$@"
+yt-dlp --format bestaudio --continue --ignore-errors --no-overwrites --output '%(uploader_id)s/%(upload_date)s_%(title)s_%(id)s.%(ext)s' --extract-audio --audio-format opus "$@"
 

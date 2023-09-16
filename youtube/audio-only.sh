9a4f7140bdd923c2cd59984dd855e7af075adc76
Andrei Rybak <rybak.a.v@gmail.com>
2023-09-16 14:05:30 +0200

    youtube: audio-only.sh: add --restrict-filenames

--- youtube/audio-only.sh
+++ youtube/audio-only.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-yt-dlp --format bestaudio --continue --ignore-errors --no-overwrites --output '%(uploader_id)s/%(upload_date)s_%(title)s_%(id)s.%(ext)s' --extract-audio --audio-format opus "$@"
+# --restrict-filenames is to avoid weird Unicode causing issues with file
+# transfers to other filesystems and such
+yt-dlp --continue --ignore-errors --no-overwrites \
+	--format bestaudio --extract-audio --audio-format opus \
+	--output '%(uploader_id)s/%(upload_date)s_%(title)s_%(id)s.%(ext)s' \
+	--restrict-filenames \
+	"$@"
 

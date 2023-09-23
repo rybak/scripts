df32aa2dff30d3bbbbb0c23b396ccc12dd96e26a
Andrei Rybak <rybak.a.v@gmail.com>
2023-09-23 16:57:10 +0200

    check_wikitext.sh: use trurl for template URLs
    Template URLs, like https://en.wikipedia.org/wiki/Template:H%C3%A5kan_Hellstr%C3%B6m need to
    be URL-encoded, which means that regular Bash string substitution is not
    enough.  Use `trurl` tool to construct the URLs for templates.

--- wikipedia/check_wikitext.sh
+++ wikipedia/check_wikitext.sh
@@ -31,7 +31,12 @@ curl "$url" |
 while read i
 do
 	escaped=${i// /_}
-	URL="https://${BASE_HOST}/w/api.php?action=parse&format=json&page=${escaped}&prop=wikitext&formatversion=2" 
+	URL=$(trurl \
+		--set host=$BASE_HOST --set scheme=https --set path=/w/api.php \
+		--append query=action=parse --append query=format=json \
+		--append query="page=${escaped}" --append query=prop=wikitext \
+		--append query=formatversion=2 \
+	)
 	wikitext=$(curl --silent "$URL" | jq --raw-output '.parse.wikitext')
 	echo -e "$i"
 	echo -e "\t$(echo ${wikitext:0:100} | grep -o '[{][{] *[^|}]*')"

9defdb771ff179b0656e28c3231e9d01bf254ea8
Andrei Rybak <rybak.a.v@gmail.com>
2023-09-25 01:10:13 +0200

    check_wikitext.sh: add expected_template parameter

--- wikipedia/check_wikitext.sh
+++ wikipedia/check_wikitext.sh
@@ -10,8 +10,14 @@ then
 	echo "Missing category name"
 	exit 1
 fi
+if [[ $# -lt 2 ]]
+then
+	echo "Missing expected template"
+	exit 1
+fi
 
 category="Category:${1// /_}"
+expected_template="$2"
 
 echo "Checking '$category'..."
 
@@ -38,9 +44,15 @@ do
 		--append query=formatversion=2 \
 	)
 	wikitext=$(curl --silent "$URL" | jq --raw-output '.parse.wikitext')
-	echo -e "$i"
-	echo -e "\t$(echo ${wikitext:0:100} | grep -o '[{][{] *[^|}]*')"
-	echo -e "\t${wikitext: -20}"
-	echo "-------------------------"
+	wikitext_start="$(echo ${wikitext:0:100} | grep -o '[{][{] *[^|}]*')"
+	if echo "$wikitext_start" | grep -q -e "$expected_template"
+	then
+		:
+	else
+		echo "${i}"
+		echo -e "\t${wikitext_start}"
+		echo -e "\t${wikitext: -20}"
+		echo "-------------------------"
+	fi
 	sleep 1s
 done

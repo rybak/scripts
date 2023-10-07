9ee8b0797817038b37ca7b83a691d1e0c06828ae
Andrei Rybak <rybak.a.v@gmail.com>
2023-10-08 00:43:17 +0200

    youtube/music.sh: new script
    New script music.sh is similar to youtube/audio-only.sh, but uses vorbis
    instead of opus for the `--audio-format` option (i.e. codec).

---
+++ youtube/music.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# --restrict-filenames is to avoid weird Unicode causing issues with file
+# transfers to other filesystems and such
+yt-dlp --continue --ignore-errors --no-overwrites \
+	--format bestaudio --extract-audio --audio-format vorbis \
+	--output '%(uploader_id)s/%(upload_date)s_%(title)s_%(id)s.%(ext)s' \
+	--restrict-filenames \
+	"$@"
+

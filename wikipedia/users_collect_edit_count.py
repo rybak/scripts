f5c1400b9f6fb654e9c250cd3e67e0a36e2d9c2e
Andrei Rybak <rybak.a.v@gmail.com>
2024-02-18 00:57:35 +0100

    users_collect_edit_count: new script for wikipedia

---
+++ wikipedia/users_collect_edit_count.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+
+from sys import stdin
+# Only in python 3.12: from itertools import batched
+import requests
+import time
+import sys
+
+
+# the separator and the limit are documented here:
+#     https://en.wikipedia.org/w/api.php?action=help&modules=query%2Busers
+USERNAMES_SEPARATOR = '|'
+LIST_USERS_LIMIT = 50
+API_BASE_URL = 'https://en.wikipedia.org/w/api.php'
+# https://stackoverflow.com/a/16549381/1083697
+sys.stdin.reconfigure(encoding='utf-8')
+
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+
+def batch_to_ususers(batch):
+    return USERNAMES_SEPARATOR.join(batch)
+
+
+def simple_write_strings(filename, lst):
+    with open(filename, 'w', encoding='utf-8') as outfile:
+        outfile.write(('\n'.join(str(i) for i in lst)) + '\n')
+
+
+def try_n_times(n, f):
+    try_count = 0
+    delay_seconds = 5
+    while try_count < n:
+        try:
+            try_count = try_count + 1
+            return f()
+        except Exception as e:
+            print('FAILED', e)
+            print('sleeping for', delay_seconds, 'seconds')
+            time.sleep(delay_seconds)
+            delay_seconds = delay_seconds * 2
+    sys.exit(1)
+
+
+usernames = []
+
+for line in stdin:
+    usernames.append(line.rstrip())
+
+edit_counts = {}
+
+for batch in chunks(usernames, LIST_USERS_LIMIT):
+    # Example: https://en.wikipedia.org/w/api.php?action=query&list=users&ususers=ZARDIAZ|Zarina%206022|Zatrp&usprop=editcount&format=json
+    ususers = batch_to_ususers(batch)
+    print(ususers)
+    params = {
+            'action':'query',
+            'list':'users',
+            'format':'json',
+            'usprop':'editcount',
+            'ususers': ususers
+            }
+    try:
+        response = try_n_times(5, lambda: requests.get(API_BASE_URL, params, timeout=10)).json()
+        users = response['query']['users']
+        for user in users:
+            if 'editcount' in user:
+                edit_counts[user['name']] = user['editcount']
+            else:
+                print('beware', user['name'])
+        time.sleep(2)
+    except Exception as e:
+        print('Unexpected exception', e)
+        sys.exit(2)
+
+
+zeros = []
+nonzeros = []
+weird = []
+
+for username in usernames:
+    if username in edit_counts:
+        if edit_counts[username] == 0:
+            zeros.append(username)
+        else:
+            nonzeros.append(username)
+    else:
+        print('ACHTUNG', username)
+        weird.append(username)
+        nonzeros.append(username)
+
+
+simple_write_strings('/tmp/zeros.txt', zeros)
+simple_write_strings('/tmp/nonzeros.txt', nonzeros)
+simple_write_strings('/tmp/weird.txt', weird)
+simple_write_strings('/tmp/edit_counts.csv', [username + ',' + str(ec) for username, ec in edit_counts.items()])
